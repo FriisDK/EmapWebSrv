0a116876b241e557db5fb70da4701baedf3eb201
Erik Friis Jensen <erik@friis-spaniel.dk>
2021-08-30 18:40:38 +0200

    div

--- EmapServer/Controllers/LicenseController.cs
+++ EmapServer/Controllers/LicenseController.cs
@@ -1,12 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Text;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 using EMAPContext;
 using EmapModel;
 using EmapServer.Models;
@@ -129,8 +123,7 @@ namespace EmapServer.Controllers
                 throw;
             }
         }
-
-
+        
         [HttpGet("UpdateCustomer")]
         public IActionResult UpdateCustomer()
         {
